cd507f8f3af9a7f488827f773ecfcd13d6aac021
Zaina202 <92650788+Zaina202@users.noreply.github.com>
2023-04-15 00:42:55 +0300

    Add Skeleton

---
+++ Dinein_ResturantApp/Dinein_ResturantApp/Dinein_ResturantApp/Views/DeatailPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Dinein_ResturantApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DeatailPage : ContentPage
+    {
+        public DeatailPage()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
---
+++ Dinein_ResturantApp/Dinein_ResturantApp/Dinein_ResturantApp/Views/OrdersPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Dinein_ResturantApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OrdersPage : ContentPage
+    {
+        public OrdersPage()
+        {
+            InitializeComponent();
+        }
+        private void Open_Detail(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DeatailPage());
+
+        }
+    }
+}
