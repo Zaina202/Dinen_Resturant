74926542b33d6507aba882126db889f37ed3b813
deemashahawan <dshahwan42@gmail.com>
2023-04-26 11:39:32 -0700

    replace file

---
+++ Dinein_ResturantApp/Dinein_ResturantApp/Dinein_ResturantApp/Services/DataBase.cs
@@ -0,0 +1,75 @@
+﻿using Dinein_ResturantApp.Models;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Database.Query;
+using FirebaseAdmin.Auth;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Dinein_UserApp.Services
+{
+    public class DataBase
+    {
+        private readonly string FirebaseClientUrl = "https://dine-in2-default-rtdb.firebaseio.com/";
+        private readonly string FirebaseSecretKey = "pVOv2WoG1nNrDAZsbmzV8OPS51oPcgdntCXDqjHK";
+
+        private FirebaseClient _firebaseClient;
+        private FirebaseAuth _firebaseAuth;
+        public DataBase()
+        {
+            _firebaseClient = new FirebaseClient(FirebaseClientUrl, new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(FirebaseSecretKey)
+            });
+
+            _firebaseAuth = FirebaseAuth.DefaultInstance;
+        }
+
+        public async Task<List<ReservationModel>> GetAllReservations()
+        {
+            
+            var reservationList = new List<ReservationModel>();
+            var reservationModels = await _firebaseClient.Child(nameof(ReservationModel)).OnceAsync<ReservationModel>();
+
+            foreach (var reservationModel in reservationModels)
+            {
+                var reservation = reservationModel.Object;
+
+                if (reservation != null && !string.IsNullOrEmpty(reservation.UserId))
+                {
+                    var userQueryResult = await _firebaseClient.Child("Users")
+                    .OrderBy("Id")
+                    .EqualTo(reservation.UserId)
+                    .OnceAsync<Users>();
+
+                    if (userQueryResult.Any())
+                    {
+                        var user = userQueryResult.First().Object;
+                        reservation.UserName = user.UserName;
+                        reservation.UserEmail = user.Email;
+                        Console.WriteLine("hiiiiiii" + user.UserName);
+
+                    }
+                   
+                }
+
+                reservationList.Add(reservation);
+                
+            }
+
+            return reservationList;
+          
+        }
+
+
+
+
+
+    }
+}
--- Dinein_ResturantApp/Dinein_ResturantApp/Dinein_ResturantApp/ViewModels/OrdersViewModel.cs
+++ Dinein_ResturantApp/Dinein_ResturantApp/Dinein_ResturantApp/ViewModels/OrdersViewModel.cs
@@ -1,10 +1,51 @@
-﻿using System;
+﻿using Dinein_ResturantApp.Models;
+using Dinein_UserApp.Services;
+using FirebaseAdmin.Auth;
+using MvvmHelpers;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Dinein_ResturantApp.ViewModels
 {
-    class OrdersViewModel
+    class OrdersViewModel : INotifyPropertyChanged
     {
+        private DataBase dataBase;
+
+        private List<ReservationModel> _ReservationItems;
+
+        public List<ReservationModel> ReservationItems
+        {
+            get { return _ReservationItems; }
+            set
+            {
+                _ReservationItems = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ReservationItems)));
+            }
+        
+        }
+
+        public OrdersViewModel()
+        { 
+            dataBase = new DataBase();
+            LoadReservationItems();
+        }
+
+        private async void LoadReservationItems()
+        {
+            ReservationItems = await dataBase.GetAllReservations();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+      
+
     }
 }
