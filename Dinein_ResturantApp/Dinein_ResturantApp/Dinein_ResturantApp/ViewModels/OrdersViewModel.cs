27d29a30cc97d2ccb128b106f74e97fdee11b210
baraaSulaiby <baraa.sulaiby@gmail.com>
2023-05-17 14:05:27 +0300

    edit

--- Dinein_ResturantApp/Dinein_ResturantApp/Dinein_ResturantApp/ViewModels/OrdersViewModel.cs
+++ Dinein_ResturantApp/Dinein_ResturantApp/Dinein_ResturantApp/ViewModels/OrdersViewModel.cs
@@ -1,9 +1,12 @@
+
 using Dinein_ResturantApp.Models;
 using Dinein_ResturantApp.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Dinein_ResturantApp.ViewModels
 {
@@ -37,11 +40,34 @@ namespace Dinein_ResturantApp.ViewModels
                 }
             }
         }
+        public ICommand RefreshCommand { get; }
+
         public OrdersViewModel()
         {
             dataBase = new DataBase();
             LoadReservationItems();
+            RefreshCommand = new Command(Refresh);
+
         }
+
+        public void Refresh()
+        {
+            LoadReservationItems();
+            IsRefreshing = false;
+        }
+
+        private bool isRefreshing;
+        public bool IsRefreshing
+        {
+            get { return isRefreshing; }
+            set
+            {
+                isRefreshing = value;
+                OnPropertyChanged(nameof(IsRefreshing));
+            }
+        }
+
+
         public OrdersViewModel(string userName)
         {
             UserName = userName;
